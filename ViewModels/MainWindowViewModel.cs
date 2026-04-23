4425ff07aad2d543d36fc9732c248c80d06a7ec4
Mianta23 <rajerymianta@gmail.com>
2026-04-23 14:25:48 -0400

    migration projet

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AvaloniaApplication1.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Bonjour [nom utilisateur] !";
+
+        // Ajout de la propriété pour le contenu dynamique (le milieu de votre fenêtre)
+        // Pour l'instant, on peut la laisser à null ou lui donner un objet par défaut
+        public object? CurrentPageContent { get; set; }
+    }
+}
